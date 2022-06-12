090fc3df60ebdc275d4ae52614ef6b540356ef98
1-eyeddd <seisyuu43@icloud.com>
2022-06-12 14:32:41 +0900

    Initial Commit

---
+++ swiftui-mvvm-sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-mvvm-sample
+//
+//  Created by Tsubata Seisyu on 2022/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-mvvm-sample/swiftui_mvvm_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mvvm_sampleApp.swift
+//  swiftui-mvvm-sample
+//
+//  Created by Tsubata Seisyu on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mvvm_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
